0250f9e3228ec46e67db39570fbc839afb532721
huangqiaoli <3351188@qq.com>
2021-10-26 18:24:46 +0800

    创建分支

--- HelloGit/src/main/java/Hello/Person.java
+++ HelloGit/src/main/java/Hello/Person.java
@@ -4,5 +4,6 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("更新1");
+        System.out.println("创建分支");
     }
 }
